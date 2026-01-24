3914de57dc6bfbf5c68a32f45f2602d9f2786f9b
EVERLOSE.CC <romaniatrotmania@gmail.com>
2026-01-24 01:08:56 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,11 +1,11 @@
-local Repository = 'https://github.com/BOCCHIROCKSSSS/Everlose/new/main/'
+local Repository = 'https://raw.githubusercontent.com/BOCCHIROCKSSSS/Everlose/refs/heads/main/'
 
 local function L_File(Repo:string, Path:string)
-	return loadstring(game:HttpGet(Repo..'/'..Path))
+	return loadstring(game:HttpGet(`{Repo}/{Path}`))()
 end
 
 if game.GameId == 3326279937 then
-	L_File(Repository, 'Blackout')
+	L_File(Repository, 'Blackout.lua')
 	return 'Blackout Loaded!'
 else
 	return 'No Loader found.'

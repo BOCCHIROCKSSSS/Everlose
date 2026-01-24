b0716d09ff345d444b68b501db1641b363fc9b8f
EVERLOSE.CC <romaniatrotmania@gmail.com>
2026-01-24 01:00:03 -0300

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,9 @@
+local Repository = 'https://github.com/BOCCHIROCKSSSS/Everlose/new/main/'
+
+local function L_File(Repo:string, Path:string)
+	return loadstring(game:HttpGet(Repo..'/'..Path))
+end
+
+if game.GameId == 3326279937 then
+	L_File(Repository, 'Blackout')
+end
